1ff05cb8c1a87d1a143c983080045e48ae3c7cd3
shalini2495 <shalini.ichha@gmail.com>
2016-02-06 16:05:31 +0530

    Create intro.java

---
+++ intro.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+class intro
+public static void main(Srring args[]){
+System.out.println("enter your name   ");
+Scanner name=new Scanner(System.in);
+System.out.println(name);
+}
